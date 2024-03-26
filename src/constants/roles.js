de74eccf4442d3f528cde7c6545d294e042873cb
Muhd Saiful <57882573+MuhdSaifulnizam15@users.noreply.github.com>
2024-03-27 00:10:56 +0800

    fix: resolve conflict and version compatability

---
+++ src/constants/roles.js
@@ -0,0 +1,21 @@
+const roles = [
+  {
+    id: 1,
+    name: 'staff',
+    requireAdminPrivilege: false,
+  },
+  {
+    id: 2,
+    name: 'manager',
+    requireAdminPrivilege: true,
+  },
+  // {
+  //   id: 3,
+  //   name: 'admin',
+  //   requireAdminPrivilege: true,
+  // }
+];
+
+export {
+  roles,
+};

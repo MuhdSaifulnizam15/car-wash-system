5295ffbee1bf42cc07f26810b5555158be197472
Muhd Saiful <57882573+MuhdSaifulnizam15@users.noreply.github.com>
2026-05-15 17:23:14 +0800

    fix: update production API URL in config

--- src/constants/config.js
+++ src/constants/config.js
@@ -10,7 +10,7 @@ const configuration = {
         apiUrl: 'http://localhost:3001/api/v1/', // dev env using local endpoint
     },
     prod: {
-        apiUrl: 'https://api.rolexbarbershop.com/api/v1/',
+        apiUrl: 'https://152.42.217.143/api/v1/',
     }
 };
 
